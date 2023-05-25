f511969953b2a0c6bfb400e464965c48ef3be19f
jorger-delfosti <jorge.rojas@delfosti.com>
2023-05-25 00:30:21 -0500

    No restaurants message

--- modules/home/components/RestaurantList.tsx
+++ modules/home/components/RestaurantList.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { IListRestaurantResponse } from '../../../common/interfaces/restaurant.interface';
 import { RestaurantService } from '../../../services/RestaurantService';
 import { SingleRestaurant } from './SingleRestaurant';
+import { Ionicons } from '@expo/vector-icons';
 
 interface IProps {
   search: string;
@@ -24,6 +25,28 @@ const RestaurantList: FC<IProps> = ({ search, categorySelected }) => {
       refetchOnWindowFocus: false,
     }
   );
+
+  if (
+    (!isLoading || !isFetching) &&
+    !isError &&
+    restaurants &&
+    restaurants?.data
+      .filter((e) =>
+        e.attributes.name.toLowerCase().includes(search.toLowerCase())
+      )
+      .filter((e) =>
+        e.attributes.categories.data
+          .map((e) => e.attributes.title)
+          .includes(categorySelected)
+      ).length === 0
+  ) {
+    return (
+      <View className="w-full h-full items-center pt-36 flex flex-col">
+        <Ionicons name="sad-outline" size={24} />
+        <Text>No se encontraron restaurantes</Text>
+      </View>
+    );
+  }
 
   return (
     <View className="w-full">
@@ -48,12 +71,6 @@ const RestaurantList: FC<IProps> = ({ search, categorySelected }) => {
               />
             );
           })}
-      {(!isLoading || !isFetching) &&
-        !isError &&
-        restaurants &&
-        restaurants?.data.filter((e) =>
-          e.attributes.name.toLowerCase().includes(search.toLowerCase())
-        ).length === 0 && <Text>No se encontraron restaurantes</Text>}
     </View>
   );
 };
